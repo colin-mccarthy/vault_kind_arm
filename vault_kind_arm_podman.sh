92b337ae8fe6bbcdec0f3d7256b6d7bece30e397
Colin McCarthy <colin-mccarthy@users.noreply.github.com>
2023-01-22 00:48:09 -0500

    Update vault_kind_arm_podman.sh

--- vault_kind_arm_podman.sh
+++ vault_kind_arm_podman.sh
@@ -12,7 +12,7 @@ KIND_CLUSTER_NAME="vault"
 echo "> initializing Kind cluster: ${KIND_CLUSTER_NAME}"
 
 # create a cluster
-cat <<EOF |  KIND_EXPERIMENTAL_PROVIDER=podman kind create cluster  --image rossgeorgiev/kind-node-arm64:v1.20.0 --name "${KIND_CLUSTER_NAME}" --config=-
+cat <<EOF |  KIND_EXPERIMENTAL_PROVIDER=podman kind create cluster  --image kindest/node:v1.23.13 --name "${KIND_CLUSTER_NAME}" --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
