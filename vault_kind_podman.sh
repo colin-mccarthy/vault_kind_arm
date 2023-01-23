86aeee664da5e38b17f6fb058c28a5cadb479e34
Colin McCarthy <colin-mccarthy@users.noreply.github.com>
2023-01-22 21:52:25 -0500

    Update vault_kind_podman.sh

--- vault_kind_podman.sh
+++ vault_kind_podman.sh
@@ -19,3 +19,42 @@ nodes:
 - role: control-plane
 EOF
 
+
+
+echo " ✓ helm repo add hashicorp https://helm.releases.hashicorp.com 📦 "
+##helm
+helm repo add hashicorp https://helm.releases.hashicorp.com 
+
+
+
+echo "> 😊😊 Verify Cluster install"
+
+sleep 30
+
+echo "> 😊😊 Verify Cluster install...."
+
+sleep 30
+
+echo "> 😊😊 Verify Cluster install...."
+
+sleep 30
+
+kubectl wait --for=condition=Ready=true node/vault-control-plane --timeout=30s
+
+
+
+echo " ✓ kubectl create ns vault 📦 "
+
+## Consul
+#
+#helm install consul hashicorp/consul --set global.name=consul --create-namespace --namespace vault --version 0.39.0
+#
+
+
+helm install vault hashicorp/vault --create-namespace --namespace vault --set='ui.enabled=true' --set='ui.serviceType=NodePort' --set 'server.dev.enabled=true'
+
+sleep 30
+
+echo "> done! 📦 "
+
+kubectl get pods -n vault
